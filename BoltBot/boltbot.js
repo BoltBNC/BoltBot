cda6f838fc3ff4503991cae10590ffcc43ea9c1f
Chaoyi Zha <summermontreal@gmail.com>
2014-11-21 19:01:28 -0500

    pull in from downstream

--- BoltBot/boltbot.js
+++ BoltBot/boltbot.js
@@ -1,17 +1,54 @@
 #!/usr/bin/env node
 
+/*
+	BoltBot Channel Manager
+	http://boltbnc.me
+	http://github.com/cydrobolt/boltbnc
+	=======----------=========
+	Copyright 2014 Chaoyi Zha
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	    http://www.apache.org/licenses/LICENSE-2.0
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+
 var irc = require('irc');
 var S = require('string');
 var sync = require('synchronize');
-
+var mongoose = require('mongoose');
+var config = require('./config.json');
 var admins = ["fedora/cydrobolt"];
+var reserved = "cydrobolt";
 
-var bot = new irc.Client('amethyst.boltbnc.me', 'BoltBNCBot', {
-	port: 7697,
+var sendgrid  = require('sendgrid')(config.sgAPIUser, config.sgAPIPassword);
+var make_passwd;
+make_passwd = function(n, a) {
+  var index = (Math.random() * (a.length - 1)).toFixed(0);
+  return n > 0 ? a[index] + make_passwd(n - 1, a) : '';
+};
+
+mongoose.connect(config.mongoConnect);
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+  console.log("Connected to MongoDB server");
+});
+
+var Request = mongoose.model('Request', {username: String, email: String, actKey: String});
+
+var bot = new irc.Client(config.ircHost, 'BoltBNCBot', {
+	port: config.ircPort,
     debug: true,
 	secure: true,
     channels: ['#BoltBNC', '#BoltBNC-test'],
-    password: ''
+    password: config.ircPassword
 });
 
 bot.addListener('error', function(message) {
@@ -39,12 +76,116 @@ bot.addListener('message', function (from, to, message, info) {
     if ( to.match(/^[#&]/) ) {
         // channel message
 		if ( message == "!request" ) {
-			bot.say(to, "So you want a Bouncer, eh?");
+			bot.notice(from, "Format: !request <username> <email>. Please wait after you request a bouncer. A staff member will approve your account, and you will receive an email with account information.");
+			return;
+		}
+
+		if ( message.match(/!request ([a-zA-Z0-9_\-[\]{}^`|]*?)\s([a-zA-Z0-9-@_]*)/) ) {
+			// Error: User [cydrobolt] already exists!
+			var regEx = /!request ([a-zA-Z0-9_\-[\]{}^`|]*?)\s([a-zA-Z0-9-@.\-_]*)/;
+			try {
+				var username = message.match(regEx)[1];
+				if (username == reserved) {
+					bot.notice(from, "Sorry, but this username is not allowed.");
+					return;
+				}
+				var email = message.match(regEx)[2];
+				bot.notice(from, "Your account has been placed on the waiting \
+				list. If it is approved, you will be PMed and emailed your credentials. \
+				Username: "+username+", Email: "+email);
+				var actKey = make_passwd(20, 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890');
+				var newUser = new Request({ username: username, email: email, actKey: actKey});
+				newUser.save(function (err) {
+					if (err) {
+						bot.notice(from, "An error occured. Please let Admins know in #BoltBNC.");
+					}
+					else {
+						bot.notice(from, "Success. You will be contacted soon.");
+					}
+				});
+				bot.say(to, "Your account is now pending. Your unique request key: "+actKey);
+
+			}
+			catch (err) {
+				console.error('Regex error in kick');
+			}
+			return;
+		}
+		if ( message.match(/!approve ([a-zA-Z0-9_\-[\]{}^`|]*?)/) ) {
+			if (isAdmin(info.host)) {
+				var desID = message.match(/!approve ([a-zA-Z0-9]*)/)[1];
+				console.log(desID);
+				var targEmail, targUsername, targPassword;
+				targPassword = make_passwd(20, 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#$%^&*()');
+				try {
+		            Request.find({ actKey: desID }, function (err, dobj) {
+
+		                var map = dobj.map(function(s){
+							targEmail = s.email;
+							targUsername = s.username;
+						});
+						if (targUsername == undefined || targUsername == "undefined") {
+							bot.say(to, "Could not find user not associated with activation key.");
+							return;
+						}
+
+						if (err) {
+							bot.notice(from, "Error specified finding user.");
+							return;
+						}
+						var payload   = {
+							to      : targEmail,
+							from    : 'no-reply@boltbnc.me',
+							subject : 'BoltBNC Account Confirmation',
+							text    : 'Hello.'+"\n"+' Your new password is '+targPassword+' for username '+targUsername+'.'+"\n"+'\
+										Webadmin: https://amethyst.boltbnc.me:7697 '+"\n"+' Server: amethyst.boltbnc.me '+"\n"+'Port: 7697 SSL, 7667 No SSL'+"\
+										\n Server Password Format: [username]/[network]:[password]",
+							html    : 'Hello.'+"<br />"+' Your new password is <b>'+targPassword+'</b> for username <b>'+targUsername+'</b>.'+"<br />"+'\
+											Webadmin: <a href="https://amethyst.boltbnc.me:7697">https://amethyst.boltbnc.me:7697</a> '+"<br />"+' Server: amethyst.boltbnc.me '+"<br />"+'Port: 7697 SSL, 7667 No SSL'+"\
+											<br /> Server Password Format: [username]/[network]:[password]"
+						}
+
+						bot.say('*controlpanel', "ADDUSER "+targUsername+" "+targPassword);
+						bot.say('*controlpanel', "SET DenySetBindHost "+targUsername+" true");
+						sendgrid.send(payload, function(err, json) {
+							if (err) { console.error(err);bot.say(to, from+", an error occured while sending the email. ");return; }
+							console.log(json);
+						});
+						try {
+				            Request.remove({ actKey: desID }, function (err) {
+				                if (err) {
+				                    bot.notice(from, "There was an error deleting the request from database.");
+				                }
+				            });
+				        }
+				        catch (err) {
+				            bot.notice(from, "There was an error deleting the request from database.")
+				        }
+						bot.say(to, "User successfully created. "+targUsername+", check your inbox for your credentials.");
+
+
+		            });
+		        }
+		        catch (err) {
+		            bot.notice(from, "Error finding specified user.");
+					return;
+		        }
+
+
+			}
+			else {
+				bot.notice(from, "Only administrators may use this command.");
+			}
+
+		return;
+		}
+		if ( message.match(/!request/) ) {
+			bot.notice(from, "Format: !request <username> <email>. Usernames must be alphanumeric, dashes and underscores are allowed. Please wait after you request a bouncer. A staff member will approve your account, and you will receive an email with account information.");
+			return;
 		}
 
 		if ( message.match(/!kick/) ) {
 			var isAllowed = isAdmin(info.host);
-			console.log("ISALLOWED?"+isAllowed);
 			if ( isAllowed == true ) {
 				var regEx = /!kick ([a-zA-Z0-9_\-[\]{}^`|]*)/;
 				try {
@@ -63,7 +204,6 @@ bot.addListener('message', function (from, to, message, info) {
 		if ( message.match(/!kban/) ) {
 			var isAllowed = isAdmin(info.host);
 
-			console.log("ISALLOWED?"+isAllowed);
 
 			if ( isAllowed == true ) {
 				try {
